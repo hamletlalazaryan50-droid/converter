902f3ad8fda4a960321d3bed39931230286f4d8b
hamletlalazaryan50-droid <hamletlalazaryan50@gmail.com>
2026-08-08 02:45:28 +0400

    created lot things about in design and some logic about price

--- js/script.js
+++ js/script.js
@@ -3,6 +3,36 @@
 const amdInput = document.getElementById("amd");
 const usdInput = document.getElementById("usd");
 
+function getRates() {
+  let request = new XMLHttpRequest();
+  request.open(
+    "GET",
+    "https://v6.exchangerate-api.com/v6/088177043da085e493e458ff/latest/USD",
+  );
+
+  request.onload = function () {
+    if (request.status === 200) {
+      let data = JSON.parse(request.response);
+      let rates = data.conversion_rates;
+
+      printPrices(rates);
+    }
+  };
+  request.send();
+}
+
+function printPrices(rates) {
+  document.querySelector(".usd").textContent = rates.AMD.toFixed(2);
+  document.querySelector(".eur").textContent = (rates.AMD / rates.EUR).toFixed(
+    2,
+  );
+  document.querySelector(".rub").textContent = (rates.AMD / rates.RUB).toFixed(
+    2,
+  );
+}
+
+getRates();
+
 amdInput.addEventListener("input", () => {
   const request = new XMLHttpRequest();
   request.open(
